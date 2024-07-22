1a879e85f5450d166c992f9e5dd27959ae0c7f78
DyiLex <1305026192@qq.com>
2024-07-22 15:33:50 +0800

    优化重构一些东西

--- mods/ip-location-info/action.js
+++ mods/ip-location-info/action.js
@@ -1,45 +1,57 @@
-!(function() {
-    let key = ILI_CONFIG.amap_api_key
+let key = CONFIG_ILI.amap_api_key
 
-    if (!axios) {
-        _hbp_logger.error('axios is not loaded!')
+if (!axios) {
+    _hbp_logger.error('axios is not loaded!')
+    return
+}
+
+if (!key) {
+    _hbp_logger.error('amap_api_key is not configured!')
+    return
+}
+
+const calcDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371; // 地球半径，单位为公里
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // 距离单位为公里
+    return distance;
+}
+
+const KEY_IP_LOCATION_INFO = 'KEY_IP_LOCATION_INFO'
+let loctaionDom = document.getElementById('ip-location-info-loc')
+let distanceDom = document.getElementById('ip-location-info-distance')
+let liveLat = CONFIG_ILI.live_lat_lng[0]
+let liveLng = CONFIG_ILI.live_lat_lng[1]
+
+let render = (locInfo) => {
+    let lngLat1 = locInfo.rectangle.split(';')[0]
+    let lngLat2 = locInfo.rectangle.split(';')[1]
+    let lng1 = lngLat1.split(',')[0] - 0
+    let lat1 = lngLat1.split(',')[1] - 0
+    let lng2 = lngLat2.split(',')[0] - 0
+    let lat2 = lngLat2.split(',')[1] - 0
+
+    loctaionDom.innerText = locInfo.province + locInfo.city
+    distanceDom.innerText = calcDistance(liveLat, liveLng, (lat1 + lat2) / 2, (lng1 + lng2) / 2).toFixed(2)
+}
+
+let loadData = () => {
+    let cache = sessionStorage.getItem(KEY_IP_LOCATION_INFO)
+    if (cache) {
+        render(JSON.parse(cache))
         return
     }
-
-    if (!key) {
-        _hbp_logger.error('amap_api_key is not configured!')
-        return
-    }
-
-    const calcDistance = (lat1, lon1, lat2, lon2) => {
-        const R = 6371; // 地球半径，单位为公里
-        const dLat = (lat2 - lat1) * Math.PI / 180;
-        const dLon = (lon2 - lon1) * Math.PI / 180;
-        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                  Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-                  Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = R * c; // 距离单位为公里
-        return distance;
-    }
-
-    let loctaionDom = document.getElementById('ip-location-info-loc')
-    let distanceDom = document.getElementById('ip-location-info-distance')
-    let liveLat = ILI_CONFIG.live_lat_lng[0]
-    let liveLng = ILI_CONFIG.live_lat_lng[1]
-
     axios.get(`https://restapi.amap.com/v3/ip?key=${key}&output=json`).then(res => {
-        let info = res.data
-        let lngLat1 = info.rectangle.split(';')[0]
-        let lngLat2 = info.rectangle.split(';')[1]
-        let lng1 = lngLat1.split(',')[0] - 0
-        let lat1 = lngLat1.split(',')[1] - 0
-        let lng2 = lngLat2.split(',')[0] - 0
-        let lat2 = lngLat2.split(',')[1] - 0
-
-        loctaionDom.innerText = info.province + info.city
-        distanceDom.innerText = calcDistance(liveLat, liveLng, (lat1 + lat2) / 2, (lng1 + lng2) / 2).toFixed(2)
+        render(res.data)
+        sessionStorage.setItem(KEY_IP_LOCATION_INFO, JSON.stringify(res.data))
     }).catch(err => {
         _hbp_logger.error(err)
     })
-})()
+}
+
+loadData()
--- mods/simple-digital-clock/action.js
+++ mods/simple-digital-clock/action.js
@@ -1,34 +1,32 @@
-!(function(){
-    if (!dayjs) {
-        console.warn('dayjs is not loaded!')
-        return
-    }
+if (!dayjs) {
+    _hbp_logger.warn('dayjs is not loaded!')
+    return
+}
 
-    let dateContainer = document.getElementById('simple-digital-clock')
-    let dateDom = dateContainer && dateContainer.children[0]
-    let timeDom = dateContainer && dateContainer.children[1]
-    let dayDom = dateContainer && dateContainer.children[2]
-    let meridiemDom
+let dateContainer = document.getElementById('simple-digital-clock')
+let dateDom = dateContainer && dateContainer.children[0]
+let timeDom = dateContainer && dateContainer.children[1]
+let dayDom = dateContainer && dateContainer.children[2]
+let meridiemDom
 
-    if (!SDC_CONFIG.time_format_24_hour) {
-        meridiemDom = dayDom.children[1]
-        dayDom = dayDom.children[0]
-    }
+if (!CONFIG_SDC.time_format_24_hour) {
+    meridiemDom = dayDom.children[1]
+    dayDom = dayDom.children[0]
+}
 
-    if (!(dateContainer && dateDom && timeDom && dayDom)) {
-        console.warn('target dom is not exist!')
-        return
-    }
+if (!(dateContainer && dateDom && timeDom && dayDom)) {
+    _hbp_logger.warn('Target dom is not exist!')
+    return
+}
 
-    let tickTock = () => {
-        let now = dayjs()
-        dateDom.innerText = now.format('M/D')
-        timeDom.innerText = now.format(meridiemDom ? 'hh:mm:ss': 'HH:mm:ss')
-        dayDom.innerText = '周' + ['日', '一', '二', '三', '四', '五', '六'][now.day()]
-        meridiemDom && (meridiemDom.innerText = now.hour() < 12? '上午': '下午')
+let tickTock = () => {
+    let now = dayjs()
+    dateDom.innerText = now.format('M/D')
+    timeDom.innerText = now.format(meridiemDom ? 'hh:mm:ss': 'HH:mm:ss')
+    dayDom.innerText = '周' + ['日', '一', '二', '三', '四', '五', '六'][now.day()]
+    meridiemDom && (meridiemDom.innerText = now.hour() < 12? '上午': '下午')
 
-        setTimeout(tickTock, 1000);
-    }
+    setTimeout(tickTock, 1000);
+}
 
-    tickTock()
-})()
+tickTock()
